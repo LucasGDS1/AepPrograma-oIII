42a76f81d2bb783d59ab6ccf8bc547699f64df01
Lucas Gabriel de Souza <lucas3.2souza@hotmail.com>
2019-04-14 17:30:13 -0300

    Add files via upload

--- aep-programacao/src/main/java/br/unicesumar/aep/aepprogramacao/TarefaRepository.java
+++ aep-programacao/src/main/java/br/unicesumar/aep/aepprogramacao/TarefaRepository.java
@@ -1,10 +1,15 @@
 package br.unicesumar.aep.aepprogramacao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa,
 Long>{
+
+	Optional<Tarefa> findById(Integer id);
+
 	
 }
